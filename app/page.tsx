6392e24adf98ab69cb73df32f1abf0723db92c1b
SameerWasHere <sbhutani95@gmail.com>
2024-10-17 14:07:34 -0700

    head fix

--- app/page.tsx
+++ app/page.tsx
@@ -23,28 +23,28 @@ export default function Home() {
     <>
       <Head>
         <title>Museum Audio Tour</title>
-        <meta name="description" content="Free museum audio tour at your fingertips." />
-        
+        <meta name="description" content="Enjoy a free museum audio tour at your fingertips." />
+
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Museum Audio Tour" />
-        <meta property="og:description" content="Free museum audio tour at your fingertips." />
-        <meta property="og:image" content="/preview.png" />
-        <meta property="og:url" content="https://museum-audio-tours.vercel.app/" />
-        <meta property="og:type" content="website" />
+          <meta property="og:title" content="Museum Audio Tour - A Free Guide" />
+          <meta property="og:description" content="Explore SFMOMA with our free audio guide. Dive deep into the world of art at your fingertips." />
+          <meta property="og:image" content="https://museum-audio-tours.vercel.app/preview.png" />
+          <meta property="og:url" content="https://museum-audio-tours.vercel.app/" />
+          <meta property="og:type" content="website" />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Museum Audio Tour" />
-        <meta name="twitter:description" content="Free museum audio tour at your fingertips." />
-        <meta name="twitter:image" content="/preview.png" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Museum Audio Tour - A Free Guide" />
+          <meta name="twitter:description" content="Explore SFMOMA with our free audio guide. Dive deep into the world of art at your fingertips." />
+          <meta name="twitter:image" content="https://museum-audio-tours.vercel.app/preview.png" />
 
         {/* Favicon and Icons */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+       </Head>
 
       <div className="flex flex-col">
         {/* Header Section */}
